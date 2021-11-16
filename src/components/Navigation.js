e2f3ca5979ba6e4490336b6eb6530a16909381f4
sandipthapa99 <sandipthapa386@gmail.com>
2021-11-16 21:46:07 +0545

    navigation bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,131 +1,38 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-
+import './navigation.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Nav, Navbar, NavDropdown} from 'react-bootstrap';
 
 function Navigation(){
         return(
             <div>
-                <div className="navigation-agileits">
-		<div className="container">
-			<nav className="navbar navbar-default">
-{/* 							<!-- Brand and toggle get grouped for better mobile display --> */}
-							<div className="navbar-header nav_2">
-								<button type="button" className="navbar-toggle collapsed navbar-toggle1" data-toggle="collapse" data-target="#bs-megadropdown-tabs">
-									<span className="sr-only">Toggle navigation</span>
-									<span className="icon-bar"></span>
-									<span className="icon-bar"></span>
-									<span className="icon-bar"></span>
-								</button>
-							</div> 
-							<div className="collapse navbar-collapse" id="bs-megadropdown-tabs">
-								<ul className="nav navbar-nav">
-									<li className="active"><Link to="/" className="act">Home</Link></li>	
-{/* 									<!-- Mega Menu --> */}
-									<li className="dropdown">
-										<a href="#" className="dropdown-toggle" data-toggle="dropdown">Groceries<b className="caret"></b></a>
-										<ul className="dropdown-menu multi-column columns-3">
-											<div className="row">
-												<div className="multi-gd-img">
-													<ul className="multi-column-dropdown">
-														<h6>All Groceries</h6>
-														<li><Link to="/groceries">Dals & Pulses</Link></li>
-														<li><Link to="/groceries">Almonds</Link></li>
-														<li><Link to="/groceries">Cashews</Link></li>
-														<li><Link to="/groceries">Dry Fruit</Link></li>
-														<li><Link to="/groceries"> Mukhwas </Link></li>
-														<li><Link to="/groceries">Rice & Rice Products</Link></li>
-													</ul>
-												</div>	
-												
-											</div>
-										</ul>
-									</li>
-									<li className="dropdown">
-										<a href="#" className="dropdown-toggle" data-toggle="dropdown">Household<b className="caret"></b></a>
-										<ul className="dropdown-menu multi-column columns-3">
-											<div className="row">
-												<div className="multi-gd-img">
-													<ul className="multi-column-dropdown">
-														<h6>All Household</h6>
-														<li><a href="household.html">Cookware</a></li>
-														<li><a href="household.html">Dust Pans</a></li>
-														<li><a href="household.html">Scrubbers</a></li>
-														<li><a href="household.html">Dust Cloth</a></li>
-														<li><a href="household.html"> Mops </a></li>
-														<li><a href="household.html">Kitchenware</a></li>
-													</ul>
-												</div>
-												
-												
-											</div>
-										</ul>
-									</li>
-									<li className="dropdown">
-										<a href="#" className="dropdown-toggle" data-toggle="dropdown">Personal Care<b className="caret"></b></a>
-										<ul className="dropdown-menu multi-column columns-3">
-											<div className="row">
-												<div className="multi-gd-img">
-													<ul className="multi-column-dropdown">
-														<h6>Baby Care</h6>
-														<li><a href="personalcare.html">Baby Soap</a></li>
-														<li><a href="personalcare.html">Baby Care Accessories</a></li>
-														<li><a href="personalcare.html">Baby Oil & Shampoos</a></li>
-														<li><a href="personalcare.html">Baby Creams & Lotion</a></li>
-														<li><a href="personalcare.html"> Baby Powder</a></li>
-														<li><a href="personalcare.html">Diapers & Wipes</a></li>
-													</ul>
-												</div>
-												
-											</div>
-										</ul>
-									</li>
-									<li className="dropdown">
-										<a href="#" className="dropdown-toggle" data-toggle="dropdown">Packaged Foods<b className="caret"></b></a>
-										<ul className="dropdown-menu multi-column columns-3">
-											<div className="row">
-												<div className="multi-gd-img">
-													<ul className="multi-column-dropdown">
-														<h6>All Accessories</h6>
-														<li><Link to="/packagedfoods">Baby Food</Link></li>
-														<li><Link to="/packagedfoods">Dessert Items</Link></li>
-														<li><Link to="/packagedfoods">Biscuits</Link></li>
-														<li><Link to="/packagedfoods">Breakfast Cereals</Link></li>
-														<li><Link to="/packagedfoods"> Canned Food </Link></li>
-														<li><Link to="/packagedfoods">Chocolates & Sweets</Link></li>
-													</ul>
-												</div>
-												
-											
-											</div>
-										</ul>
-									</li>
-									<li className="dropdown">
-										<a href="#" className="dropdown-toggle" data-toggle="dropdown">Beverages<b className="caret"></b></a>
-										<ul className="dropdown-menu multi-column columns-3">
-											<div className="row">
-												<div className="multi-gd-img">
-													<ul className="multi-column-dropdown">
-														<h6>Tea & Coeffe</h6>
-														<li><a href="beverages.html">Green Tea</a></li>
-														<li><a href="beverages.html">Ground Coffee</a></li>
-														<li><a href="beverages.html">Herbal Tea</a></li>
-														<li><a href="beverages.html">Instant Coffee</a></li>
-														<li><a href="beverages.html"> Tea </a></li>
-														<li><a href="beverages.html">Tea Bags</a></li>
-													</ul>
-												</div>
-							
-											</div>
-										</ul>
-									</li>
-									<li><Link to="/gourmet">Gourmet</Link></li>
-									<li><Link to="/offers">Offers</Link></li>
-									<li><Link to="/contact">Contact</Link></li>
-								</ul>
-							</div>
-							</nav>
-			</div>
-		</div>
+				<Navbar bg="mybg" variant="dark" sticky="top" expand="md" collapseOnSelect font="myfont">
+				<Navbar.Toggle/>
+				<Navbar.Collapse>
+				<Nav>
+					<Nav.Link><Link to="/" className="navTitle">Home</Link></Nav.Link>
+					<NavDropdown title={<span className="navTitle">Groceries</span>}>
+						<NavDropdown.Item><Link to="/groceries">Dals & Pulses</Link></NavDropdown.Item>
+						<NavDropdown.Item><Link to="/groceries">Almonds</Link></NavDropdown.Item>
+						<NavDropdown.Item><Link to="/groceries">Cashews</Link></NavDropdown.Item>
+						<NavDropdown.Item><Link to="/groceries">Dry Fruit</Link></NavDropdown.Item>
+						<NavDropdown.Item><Link to="/groceries">Mukhwas</Link></NavDropdown.Item>
+						<NavDropdown.Item><Link to="/groceries">Rice & Rice Products</Link></NavDropdown.Item>
+						<NavDropdown.Divider/>
+						<NavDropdown.Item href="/groceries">Rice & Rice Products</NavDropdown.Item>
+					</NavDropdown>
+					<NavDropdown title={<span className="navTitle">Household</span>}></NavDropdown>
+					<NavDropdown title={<span className="navTitle">Personal Care</span>}></NavDropdown>
+					<NavDropdown title={<span className="navTitle">Packaged Foods</span>}></NavDropdown>
+					<NavDropdown title={<span className="navTitle">Beverages</span>}></NavDropdown>
+					<Nav.Link href="/gourmet" id="navTitle"><Link to="/gourmet" className="navTitle">Gourmet</Link></Nav.Link>
+					<Nav.Link href="/offers" id="navTitle"><Link to="/offers" className="navTitle">Offers</Link></Nav.Link>
+					<Nav.Link><Link to="/contact" className="navTitle">Contact</Link></Nav.Link>
+				</Nav>
+				</Navbar.Collapse>	
+				
+				</Navbar>
             </div>
         );
 }
