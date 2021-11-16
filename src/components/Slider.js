fb924505573bda95fe0815a5a4ae5f4d4d67c377
sandipthapa99 <sandipthapa386@gmail.com>
2021-11-16 11:32:54 +0545

    dependencies added

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,25 +1,28 @@
 import React from 'react';
+import img1 from '../assets/images/11.jpg';
+import img2 from '../assets/images/22.jpg';
+import img3 from '../assets/images/44.jpg';
 
 function Slider(){
     return(
         <div className="skdslider">
         <ul id="demo1">
 			<li>
-				<img src='/assets/images/11.jpg' alt="" />
+				<img src={img1} alt="" />
 {/* 				<!--Slider Description example--> */}
 				<div class="slide-desc">
 					<h3>Buy Rice Products Are Now On Line With Us</h3>
 				</div>
 			</li>
 			<li>
-				<img src='/assets/images/22.jpg' alt="" />
+				<img src={img2} alt="" />
 				  <div class="slide-desc">
 					<h3>Whole Spices Products Are Now On Line With Us</h3>
 				</div>
 			</li>
 			
 			<li>
-				<img src='../assets/images/44.jpg' alt="" />
+				<img src={img3} alt="" />
 				<div class="slide-desc">
 					<h3>Whole Spices Products Are Now On Line With Us</h3>
 				</div>
