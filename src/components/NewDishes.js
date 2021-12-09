2bde56a37a99f8dca944e5d0184198ddab665842
sandipthapa99 <sandipthapa386@gmail.com>
2021-12-09 08:49:10 +0545

    react-slick responsive

--- src/components/NewDishes.js
+++ src/components/NewDishes.js
@@ -18,7 +18,40 @@ const NewDishes = () => {
         infinite: true,
         speed: 500,
         slidesToShow: 4,
-        slidesToScroll: 4
+        slidesToScroll: 4,
+        responsive:[
+            {
+                breakpoint: 1024,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 3,
+                  infinite: true,
+                  dots: true
+                }
+              },
+              {
+                breakpoint: 600,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2,
+                  initialSlide: 2
+                }
+              },
+              {
+                breakpoint: 480,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+              },
+              {
+                breakpoint: 360,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+              },
+        ]
       };
     return (
         <div className="container mb-5">
