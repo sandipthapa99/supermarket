9967a2be452d096b65af0d5a301bbd4a05a5a354
sandipthapa99 <sandipthapa386@gmail.com>
2022-01-06 12:32:18 +0545

    Revert "basic changes"
    This reverts commit a9e8eb75b988a40ab61b642980b1d21a61c526d0.

--- src/components/NewDishes.js
+++ src/components/NewDishes.js
@@ -26,7 +26,7 @@ const NewDishes = () => {
                   slidesToShow: 3,
                   slidesToScroll: 3,
                   infinite: true,
-                  dots: false
+                  dots: true
                 }
               },
               {
@@ -48,7 +48,7 @@ const NewDishes = () => {
                 breakpoint: 360,
                 settings: {
                   slidesToShow: 1,
-                  slidesToScroll: 1,
+                  slidesToScroll: 1
                 }
               },
         ]
